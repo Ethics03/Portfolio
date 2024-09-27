dacac29588f02e9cbb8b961c2c2177b71b55d67f
Rachit Srivastava <honey032004@gmail.com>
2024-09-27 22:35:09 +0530

    navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -6,9 +6,9 @@ import {FaSquareXTwitter} from 'react-icons/fa6'
 import {FaInstagram} from 'react-icons/fa'
 function Navbar() {
   return (
-    <nav className = 'mb-20 flex items-center justify-between py-6'>
-        <div className = 'flex flex-shrink-0 items-center'>
-            <img src = {logo} alt='logo'/>
+    <nav className = 'overflow-x-hidden text-neutral-300  mb-20 flex items-center justify-between py-6 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
+        <div className = 'text-2xl'>
+            It's Rachit
             </div>
         <div className = 'm-8 flex items-center justify-center gap-4 text-2xl'>
             <FaLinkedin/>
