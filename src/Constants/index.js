f7a1eb45caadbe783492e0b93bc108a2094ecc98
Rachit Srivastava <honey032004@gmail.com>
2024-09-28 20:24:21 +0530

    finalherocontent

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -1,2 +1,2 @@
- export const HERO_CONTENT = "I am a 2nd-Year CSE student from CMRIT Bangalore. I am a passionate full stack developer and loves to make scalable web applications. I have made multiple mini-projects and also working on big ones that help the society and solve real-world problems."
+ export const HERO_CONTENT = "I am a 2nd-Year CSE student from CMRIT Bangalore. I am a passionate full stack developer and love to make scalable web applications. I have made multiple mini-projects and also am working on big ones that help the society and solve real-world problems."
 
