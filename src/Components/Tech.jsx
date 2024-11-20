b44337423f8249119070414d96ac2fe396052f7a
Rachit Srivastava <honey032004@gmail.com>
2024-11-20 22:22:03 +0530

    Addedstack

--- src/Components/Tech.jsx
+++ src/Components/Tech.jsx
@@ -5,6 +5,8 @@ import { DiPostgresql } from "react-icons/di";
 import { TbBrandNextjs } from "react-icons/tb";
 import { RiNodejsLine } from "react-icons/ri";
 import { FaDocker } from "react-icons/fa";
+import { SiNestjs } from "react-icons/si";
+import { SiTypescript } from "react-icons/si";
 import { motion } from "framer-motion"
 
 const iconVariants  = (duration) => ({
@@ -35,6 +37,14 @@ const Tech = () => {
             initial={{opacity:0,x: -100}}
             transition={{duration:1.5}}
         className ='flex flex-wrap items-center justify-center gap-4'>
+            <motion.div  
+                    variants={iconVariants(5)}
+                    initial="initial"
+                    animate = "animate"
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
+                <SiTypescript className = 'text-4xl text-cyan-400'/>
+            </motion.div >
+
             <motion.div 
             variants={iconVariants(2.5)}
             initial="initial"
@@ -49,6 +59,15 @@ const Tech = () => {
             className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <FaGolang className = 'text-4xl text-cyan-400'/>
             </motion.div>
+            
+            <motion.div  
+                    variants={iconVariants(5)}
+                    initial="initial"
+                    animate = "animate"
+            className='rounded-2xl border-4 border-neutral-800 p-4'>
+                <SiNestjs className = 'text-4xl text-cyan-400'/>
+            </motion.div >
+
             <motion.div 
             variants={iconVariants(6)}
             initial="initial"
@@ -78,6 +97,11 @@ const Tech = () => {
             className='rounded-2xl border-4 border-neutral-800 p-4'>
                 <FaDocker className = 'text-4xl text-cyan-400'/>
             </motion.div >
+
+            
+
+            
+
             
         </motion.div>
         </div>
